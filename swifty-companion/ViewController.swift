f511bb73738afa085f94d17fe9e6b2586b4abbf2
Morgane DUBUS <mdubus@student.42.fr>
2019-04-07 16:09:59 +0200

    setup

---
+++ swifty-companion/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swifty-companion
+//
+//  Created by Morgane DUBUS on 4/7/19.
+//  Copyright © 2019 Morgane DUBUS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
