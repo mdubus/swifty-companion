76fe018a84b21295a095219ec9ace43a54f3512f
Morgane DUBUS <mdubus@student.42.fr>
2019-04-07 18:40:34 +0200

    retrieve token [OK]

---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'swifty-companion' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for swifty-companion
+  pod 'Alamofire', '~> 4.7'
+
+end
--- swifty-companion/ViewController.swift
+++ swifty-companion/ViewController.swift
@@ -6,20 +6,51 @@
 //  Copyright © 2019 Morgane DUBUS. All rights reserved.
 //
 
+
 import UIKit
+import Alamofire
+
+let AUTHURL = "https://api.intra.42.fr/oauth/token"
+
+struct Token: Codable {
+    let access_token: String
+}
+
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
+        
+        
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+//        print("UID", ProcessInfo.processInfo.environment["UID"]!)
+//        print("SECRET", ProcessInfo.processInfo.environment["SECRET"]!)
+        
+        guard let UID = ProcessInfo.processInfo.environment["UID"] else {return }
+        guard let SECRET = ProcessInfo.processInfo.environment["SECRET"] else {return }
+        
+        Alamofire.request(AUTHURL, method: .post, parameters: [
+            "grant_type":"client_credentials",
+            "client_id":UID,
+            "client_secret":SECRET
+            ]).response {response in
+                guard let data = response.data else { print("Unable to retrieve token"); return }
+            do {
+                let decoder = JSONDecoder()
+                let tokenData = try decoder.decode(Token.self, from: data)
+                print(tokenData.access_token)
+            } catch let error {
+                print(error)
+            }
+        }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
