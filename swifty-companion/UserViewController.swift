46cc07bd2937c41cdc270d69a6302a56aa1a4690
mdubus <mdubus@student.42.fr>
2019-04-17 08:42:34 +0200

    shadow on profile picture [OK]

--- swifty-companion/UserViewController.swift
+++ swifty-companion/UserViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class UserViewController: UIViewController {
-    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var profileImage: UIView!
     @IBOutlet weak var profileStackView: UIStackView!
     @IBOutlet weak var fullName: UILabel!
     
@@ -35,12 +35,32 @@ class UserViewController: UIViewController {
         super.viewDidLoad()
         
         print("***********************************************")
-//        print(self.user)
+        //        print(self.user)
         self.view.setGradientBackground(colorOne: sweetPink, colorTwo: sweetViolet, gradientLayer: gradientLayer)
         profileStackView.addBackground(color: .white)
         
         if let profilePicture = self.user?.imageURL, let url = URL(string: profilePicture), let data = try? Data(contentsOf: url), let userImage = UIImage(data: data){
-            image.image = userImage.circle
+            
+            let square = CGSize(width: 100, height: 100)
+            
+            profileImage.clipsToBounds = false
+            profileImage.layer.cornerRadius = square.width/2
+            profileImage.layer.shadowColor = UIColor.black.cgColor
+            profileImage.layer.shadowOpacity = 1
+            profileImage.layer.shadowOffset = CGSize.zero
+            profileImage.layer.shadowRadius = 10
+            profileImage.layer.shadowPath = UIBezierPath(roundedRect: profileImage.bounds, cornerRadius: square.width/2).cgPath
+            
+            let imageView = UIImageView(frame: CGRect(origin: CGPoint(x: 0, y: 0), size: square))
+            imageView.clipsToBounds = true
+            imageView.layer.cornerRadius = square.width/2
+            imageView.layer.borderColor = UIColor.white.cgColor
+            imageView.layer.borderWidth = 4.0
+            imageView.contentMode = .scaleAspectFill
+            imageView.image = userImage
+            
+            profileImage.addSubview(imageView)
+            
         }
         
         guard let userFirstName = self.user?.firstName, let userLastName = self.user?.lastName else {self.manageError("Missing user data : firstName or lastName"); return}
