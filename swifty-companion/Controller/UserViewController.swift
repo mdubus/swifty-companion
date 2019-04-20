dd6c18f8caaaaea1a13ad3dcfae2d2a8144386db
Morgane DUBUS <mdubus@student.42.fr>
2019-04-20 10:25:31 +0200

    refacto [OK]

--- swifty-companion/Controller/UserViewController.swift
+++ swifty-companion/Controller/UserViewController.swift
@@ -27,14 +27,53 @@ class UserViewController: UIViewController, UITableViewDataSource, UITableViewDe
     @IBOutlet weak var skillsTableView: UITableView!
     
     let gradientLayer = CAGradientLayer()
-    
     var user:User?
     var userProjects:[UserProject]?
     var userSkills:[Skill]?
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // get User Projects and Skills
+        self.userProjects = self.user?.getProjects()
+        self.userSkills = self.user?.getSkills()
+        
+        // Set Main View
+        self.view.setGradientBackground(colorOne: sweetPink, colorTwo: sweetViolet, gradientLayer: gradientLayer)
+        scrollView.backgroundColor = UIColor.clear
+        mainView.backgroundColor = UIColor.clear
+        
+        // Set Project View only if there are projects
+        self.setProfileView()
+        if let projects = self.userProjects, projects.count > 0 {
+            setTableView(tbView: projectsTableView, label: projectsLabel, view: projectsView)
+        }
+        else {
+            projectsView.isHidden = true
+        }
+        
+        // Set Skills View only if there are projects
+        if let skills = self.userSkills, skills.count > 0 {
+            setTableView(tbView: skillsTableView, label: skillsLabel, view: skillsView)
+        }
+        else {
+            skillsView.isHidden = true
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        gradientLayer.frame = view.layer.bounds
+    }
     
     func setProfileView() {
-        guard let user = user else {alert(view: self, message: "No user provided"); return}
+        
+        guard let user = user else { alert(view: self, message: "No user provided"); return }
+        
         // Set View
         profileInfosView.layer.cornerRadius = radius
         profileInfosView.setShadow(radius)
@@ -61,91 +100,19 @@ class UserViewController: UIViewController, UITableViewDataSource, UITableViewDe
         level.textColor = .white
     }
     
-    func setProjectsView() {
-        projectsTableView.delegate = self
-        projectsTableView.dataSource = self
+    // Set Table Views
+    func setTableView(tbView: UITableView, label: UILabel, view:UIView){
+        tbView.delegate = self
+        tbView.dataSource = self
         
-        projectsLabel.setTopCornerRadius(radius)
-        projectsLabel.layer.backgroundColor = sweetPink.cgColor
-        projectsLabel.textColor = .white
-        projectsLabel.text = "PROJECTS"
+        label.setTopCornerRadius(radius)
+        label.layer.backgroundColor = sweetPink.cgColor
+        label.textColor = .white
         
-        projectsView.layer.cornerRadius = radius
-        projectsView.setShadow(radius)
-        projectsTableView.setBottomCornerRadius(radius)
+        view.layer.cornerRadius = radius
+        view.setShadow(radius)
+        tbView.setBottomCornerRadius(radius)
     }
-    
-    func setSkillsView() {
-        skillsTableView.delegate = self
-        skillsTableView.dataSource = self
-        
-        skillsLabel.setTopCornerRadius(radius)
-        skillsLabel.layer.backgroundColor = sweetPink.cgColor
-        skillsLabel.textColor = .white
-        skillsLabel.text = "SKILLS"
-        
-        skillsView.layer.cornerRadius = radius
-        skillsView.setShadow(radius)
-        skillsTableView.setBottomCornerRadius(radius)
-        
-    }
-    
-    override func viewWillLayoutSubviews() {
-        super.viewWillLayoutSubviews()
-        gradientLayer.frame = view.layer.bounds
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        print("***********************************************")
-        
-        // get User Projects
-        self.userProjects = self.user?.getProjects()
-        self.userSkills = self.user?.getSkills()
-        
-        // Set Main View
-        self.view.setGradientBackground(colorOne: sweetPink, colorTwo: sweetViolet, gradientLayer: gradientLayer)
-        scrollView.backgroundColor = UIColor.clear
-        mainView.backgroundColor = UIColor.clear
-        
-        
-        // Set Project View only if there are projects
-        self.setProfileView()
-        if let projects = self.userProjects, projects.count > 0 {
-            self.setProjectsView()
-        }
-        else {
-            projectsView.isHidden = true
-            projectsView.frame.size.height = 0
-        }
-        
-        if let skills = self.userSkills, skills.count > 0 {
-            setSkillsView()
-        }
-        else {
-            skillsView.isHidden = true
-            skillsView.frame.size.height = 0
-        }
-    }
-    
-    
-    // Get project Final Mark depending on Project's Status
-    func getProjectFinalMark(_ project:UserProject) -> String {
-        guard let status = project.status else { return "No status" }
-        switch(status) {
-        case "parent": return "X"
-        case "in_progress": return "In progress"
-        case "creating_group": return "Creating group"
-        case "searching_a_group": return "Searching a group"
-        case "finished":
-            guard let finalMark = project.finalMark else { return "No status" }
-            return String(finalMark)
-        default :
-            return project.status ?? "No status"
-        }
-    }
-    
     
     // Get number of cells
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -160,7 +127,6 @@ class UserViewController: UIViewController, UITableViewDataSource, UITableViewDe
         }
         return count
     }
-
     
     // Populate Cell
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -178,7 +144,7 @@ class UserViewController: UIViewController, UITableViewDataSource, UITableViewDe
             let finalMark = getProjectFinalMark(cellProject)
             cell.detailTextLabel?.text = finalMark
             if (cellProject.status == "finished" && cellProject.validated != nil) {
-                cell.detailTextLabel?.textColor = cellProject.validated == true ? sweetGreen : UIColor.red
+                cell.detailTextLabel?.textColor = cellProject.validated == true ? sweetGreen : sweetRed
             }
             else {
                 cell.detailTextLabel?.textColor = UIColor.black
@@ -190,14 +156,27 @@ class UserViewController: UIViewController, UITableViewDataSource, UITableViewDe
             guard let skills = self.userSkills else { return cell }
             let cellSkill = skills[indexPath.row]
             cell.textLabel?.text = cellSkill.name
-            let skill = cellSkill.level
-            cell.detailTextLabel?.text = String(describing: skill ?? 0.0)
+            let skillLevel = cellSkill.level
+            cell.detailTextLabel?.text = String(describing: skillLevel ?? 0.0)
         }
         
         return cell
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+    // Get project Final Mark depending on Project's Status
+    func getProjectFinalMark(_ project:UserProject) -> String {
+        guard let status = project.status else { return "No mark" }
+        switch(status) {
+        case "parent": return "X"
+        case "in_progress": return "In progress"
+        case "creating_group": return "Creating group"
+        case "searching_a_group": return "Searching a group"
+        case "finished":
+            guard let finalMark = project.finalMark else { return "No mark" }
+            return String(finalMark)
+        default :
+            return project.status ?? "No mark"
+        }
     }
+    
 }
